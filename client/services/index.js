e8c8ff3a49f9ba34cbe5faace399359e66bae89e
Marcos <m.juniorcsebastiao@gmail.com>
2020-06-13 23:39:35 -0300

    Fixed baseURL and removed insecure line

--- client/services/index.js
+++ client/services/index.js
@@ -1,20 +1,10 @@
 import axios from "axios";
-import https from "https";
 
 export const http = axios.create({
-  baseURL: "https://localhost:5001/api", // Get from environment variables later
-  httpsAgent: new https.Agent({ rejectUnauthorized: false }) // Insecure, only for local testing
+  baseURL: "https://click-clack.cc:5000/api",
 });
 
-
 http.interceptors.request.use((config) => {
-  // Setup a way to get the token automatically  
-  // const token = getToken();
-
-    // if (token) {
-    //     config.headers.Authorization = `Bearer ${token}`;
-    // }
-
     return config;
 });
 
